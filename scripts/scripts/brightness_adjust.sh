7c093b2e0189265294d15f37dd99c5285010981a
phil0x2e <phil0x2e@posteo.org>
2025-07-18 23:46:46 +0200

    Increase expire-time

--- scripts/scripts/brightness_adjust.sh
+++ scripts/scripts/brightness_adjust.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 notification_path=/tmp/brightness_adjust_notification_id
+expire_time=750
 
 brightnessctl s "$1"
 
@@ -8,8 +9,8 @@ cur="$(brightnessctl g)"
 percentage=$((cur * 100 / max))
 
 if [ -f "$notification_path" ]; then
-  new_id="$(notify-send "󰃠" -h "int:value:$percentage" -t 500 -r "$(cat "$notification_path")" -p)"
+  new_id="$(notify-send "󰃠" -h "int:value:$percentage" -t $expire_time -r "$(cat "$notification_path")" -p)"
   echo "$new_id" >"$notification_path"
 else
-  notify-send -p "󰃠" -h "int:value:$percentage" -t 500 >"$notification_path"
+  notify-send -p "󰃠" -h "int:value:$percentage" -t $expire_time >"$notification_path"
 fi
--- scripts/scripts/volume_adjust.sh
+++ scripts/scripts/volume_adjust.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 notification_path=/tmp/volume_adjust_notification_id
+expire_time=750
 
 if [[ "$1" == "toggle mute" ]]; then
   wpctl "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle"
   if [[ "$(wpctl "get-volume" "@DEFAULT_AUDIO_SINK@")" == *"[MUTED]" ]]; then
-    notify-send "  Audio muted" -t 500
+    notify-send "  Audio muted" -t $expire_time
   else
-    notify-send "  Audio unmuted" -t 500
+    notify-send "  Audio unmuted" -t $expire_time
   fi
   exit 0
 elif [[ "$1" == "toggle mute mic" ]]; then
   wpctl "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle"
   if [[ "$(wpctl "get-volume" "@DEFAULT_AUDIO_SOURCE@")" == *"[MUTED]" ]]; then
-    notify-send " Mic unmuted" -t 500
+    notify-send " Mic unmuted" -t $expire_time
   else
-    notify-send "  Mic muted" -t 500
+    notify-send "  Mic muted" -t $expire_time
   fi
   exit 0
 fi
@@ -23,15 +24,14 @@ wpctl "set-volume" "@DEFAULT_AUDIO_SINK@" "$1"
 
 volume="$(wpctl "get-volume" "@DEFAULT_AUDIO_SINK@" | sed "s/Volume: //")"
 if [[ $volume == 0.* ]]; then
-  # capped_percentage="$(echo "$volume" | sed "s/0.//")"
   percentage="${volume//0./}"
 else
   percentage="${volume//./}"
 fi
 
 if [ -f "$notification_path" ]; then
-  new_id="$(notify-send "" -h "int:value:$percentage" -t 500 -r "$(cat "$notification_path")" -p)"
+  new_id="$(notify-send "" -h "int:value:$percentage" -t $expire_time -r "$(cat "$notification_path")" -p)"
   echo "$new_id" >"$notification_path"
 else
-  notify-send -p "" -h "int:value:$percentage" -t 500 >"$notification_path"
+  notify-send -p "" -h "int:value:$percentage" -t $expire_time >"$notification_path"
 fi
