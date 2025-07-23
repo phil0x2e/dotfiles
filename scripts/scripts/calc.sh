b1dae55ed2e94aa1366fb83b2207443cdfefc7d8
phil0x2e <phil0x2e@posteo.org>
2025-07-23 23:54:35 +0200

    Fuzzel scripts: Remove empty lines

--- scripts/scripts/calc.sh
+++ scripts/scripts/calc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-input="$(fuzzel -d -p ' > ' --placeholder='e.g. 6*7')" || exit 0
+input="$(fuzzel -d -p ' > ' --placeholder='e.g. 6*7' -l 0)" || exit 0
 notify-send "$(echo "$input" | bc -l)"
--- scripts/scripts/niri_outputs.sh
+++ scripts/scripts/niri_outputs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 outputs=$(niri msg outputs | grep -oP '(?<=Output ").+(?=")') || exit 0
-selected_output=$(echo "$outputs" | fuzzel -d -p '󰍺 > ' --placeholder="Select output") || exit 0
-selected_action=$(echo -e "󰍹 on\n󰶐 off" | fuzzel -d -p ' > ' --placeholder="Turn output on/off" | sed 's/^.* //') || exit 0
+num_outputs=$(echo "$outputs" | wc -l)
+num_lines=$((num_outputs < 15 ? num_outputs : 15)) # maximum 15 lines, which is default line number for fuzzel
+selected_output=$(echo "$outputs" | fuzzel -d -p '󰍺 > ' --placeholder="Select output" -l "$num_lines") || exit 0
+selected_action=$(echo -e "󰍹 on\n󰶐 off" | fuzzel -d -p ' > ' --placeholder="Turn output on/off" -l 2 | sed 's/^.* //') || exit 0
 
 niri msg output "$selected_output" "$selected_action"
